8760ca1f8b947bee6a217f044a7663518890ca3c
Paul Irish <paul.irish@gmail.com>
2016-02-24 15:00:24 -0800

    screenshots test file and standalone script

---
+++ get-last-screenshot.js
@@ -0,0 +1,20 @@
+const filename = 'test/devtools-homepage-w-screenshots-trace.json'
+
+var fs = require('fs')
+var traceToTimelineModel = require('.')
+
+var events = fs.readFileSync(filename, 'utf8')
+var model = traceToTimelineModel(events)
+
+console.log('Filmstrip model', model.filmStripModel)
+
+// get screenshot
+var frames = model.filmStripModel.frames()
+var length = frames.length
+if (length >= 1) {
+  frames[length - 1].imageDataPromise()
+    .then(data => Promise.resolve('data:image/jpg;base64,' + data))
+    .then(img => {
+      console.log(img)
+    })
+}
