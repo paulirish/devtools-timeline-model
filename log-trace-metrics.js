e4924890767cf8e93e3b1f7f2f4f04618c41faef
Paul Irish <paul.irish@gmail.com>
2016-02-24 15:12:00 -0800

    log-metrics: report metrics for both files.

--- log-trace-metrics.js
+++ log-trace-metrics.js
@@ -1,19 +1,26 @@
-const filename = 'test/mdn-fling.json'
-// const filename = 'test/devtools-homepage-w-screenshots-trace.json'
+const filenames = [
+  'test/mdn-fling.json',
+  'test/devtools-homepage-w-screenshots-trace.json'
+]
 
 var fs = require('fs')
 var traceToTimelineModel = require('.')
 
-var events = fs.readFileSync(filename, 'utf8')
-var model = traceToTimelineModel(events)
+function report (filename) {
+  var events = fs.readFileSync(filename, 'utf8')
+  var model = traceToTimelineModel(events)
 
-console.log('Timeline model events:', model.timelineModel.mainThreadEvents().length)
-console.log('IR model interactions', model.irModel.interactionRecords().length)
-console.log('Frame model frames', model.frameModel.frames().length)
-console.log('Filmstrip model screenshots:', model.filmStripModel.frames().length)
+  console.log('\n', filename, ':')
+  console.log('Timeline model events:', model.timelineModel.mainThreadEvents().length)
+  console.log('IR model interactions', model.irModel.interactionRecords().length)
+  console.log('Frame model frames', model.frameModel.frames().length)
+  console.log('Filmstrip model screenshots:', model.filmStripModel.frames().length)
 
-// console.log('Tracing model:', model.timelineModel._tracingModel)
-// console.log('Timeline model:', model.timelineModel)
-// console.log('IR model', model.irModel)
-// console.log('Frame model', model.frameModel)
-// console.log('Filmstrip model', model.filmStripModel)
+  // console.log('Tracing model:', model.timelineModel._tracingModel)
+  // console.log('Timeline model:', model.timelineModel)
+  // console.log('IR model', model.irModel)
+  // console.log('Frame model', model.frameModel)
+  // console.log('Filmstrip model', model.filmStripModel)
+}
+
+filenames.forEach(report)
