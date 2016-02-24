35e10c93062888963d9e701ad4d14dadd4030547
Paul Irish <paul.irish@gmail.com>
2016-02-24 14:58:58 -0800

    standard formatting

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import test from 'ava';
+import test from 'ava'
 
 var fs = require('fs')
 var traceToTimelineModel = require('../')
@@ -8,13 +8,13 @@ var events = fs.readFileSync(filename, 'utf8')
 var model
 
 test("doesn't throw an exception", t => {
-	t.notThrows(_ => {
-		model = traceToTimelineModel(events)
-	});
-});
+  t.notThrows(_ => {
+    model = traceToTimelineModel(events)
+  })
+})
 
-test("metrics returned are expected", t => {
-	t.is(model.timelineModel.mainThreadEvents().length, 4783);
-	t.is(model.irModel.interactionRecords().length, 9);
-	t.is(model.frameModel.frames().length, 125);
-});
+test('metrics returned are expected', t => {
+  t.is(model.timelineModel.mainThreadEvents().length, 4783)
+  t.is(model.irModel.interactionRecords().length, 9)
+  t.is(model.frameModel.frames().length, 125)
+})
