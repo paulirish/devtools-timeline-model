51894a949e201be6feae68710a649692a4b299dc
Paul Irish <paul.irish@gmail.com>
2016-03-17 14:47:28 -0700

    whitespace

--- index.js
+++ index.js
@@ -36,47 +36,47 @@ require('./lib/timeline-model-treeview')
 
 class TraceToTimelineModel {
 
-  constructor (events) {
-      // (devtools) tracing model
-      this._tracingModel = new WebInspector.TracingModel(new WebInspector.TempFileBackingStorage('tracing'))
-      // timeline model
-      this._timelineModel = new WebInspector.TimelineModel(WebInspector.TimelineUIUtils.visibleEventsFilter())
+  constructor(events) {
+    // (devtools) tracing model
+    this._tracingModel = new WebInspector.TracingModel(new WebInspector.TempFileBackingStorage('tracing'))
+    // timeline model
+    this._timelineModel = new WebInspector.TimelineModel(WebInspector.TimelineUIUtils.visibleEventsFilter())
 
-      // populate with events
-      this._tracingModel.reset()
-      this._tracingModel.addEvents(typeof events === 'string' ? JSON.parse(events) : events)
-      this._tracingModel.tracingComplete()
-      this._timelineModel.setEvents(this._tracingModel)
+    // populate with events
+    this._tracingModel.reset()
+    this._tracingModel.addEvents(typeof events === 'string' ? JSON.parse(events) : events)
+    this._tracingModel.tracingComplete()
+    this._timelineModel.setEvents(this._tracingModel)
 
-      this._aggregator = new WebInspector.TimelineAggregator((event) => WebInspector.TimelineUIUtils.eventStyle(event).category.name)
+    this._aggregator = new WebInspector.TimelineAggregator((event) => WebInspector.TimelineUIUtils.eventStyle(event).category.name)
 
-      return this;
+    return this;
   }
 
-  timelineModel () {
+  timelineModel() {
     return this._timelineModel;
   }
 
-  tracingModel () {
+  tracingModel() {
     return this._tracingModel;
   }
 
-  topDown () {
-      var filters = [];
-      filters.push(WebInspector.TimelineUIUtils.visibleEventsFilter());
-      filters.push(new WebInspector.ExcludeTopLevelFilter());
-      var nonessentialEvents = [
-        WebInspector.TimelineModel.RecordType.EventDispatch,
-        WebInspector.TimelineModel.RecordType.FunctionCall,
-        WebInspector.TimelineModel.RecordType.TimerFire
-      ];
-      filters.push(new WebInspector.ExclusiveNameFilter(nonessentialEvents));
+  topDown() {
+    var filters = [];
+    filters.push(WebInspector.TimelineUIUtils.visibleEventsFilter());
+    filters.push(new WebInspector.ExcludeTopLevelFilter());
+    var nonessentialEvents = [
+      WebInspector.TimelineModel.RecordType.EventDispatch,
+      WebInspector.TimelineModel.RecordType.FunctionCall,
+      WebInspector.TimelineModel.RecordType.TimerFire
+    ];
+    filters.push(new WebInspector.ExclusiveNameFilter(nonessentialEvents));
 
-      return WebInspector.TimelineProfileTree.buildTopDown(this._timelineModel.mainThreadEvents(),
-                    filters, /* startTime */ 0, /* endTime */ Infinity, WebInspector.TimelineAggregator.eventId)
+    return WebInspector.TimelineProfileTree.buildTopDown(this._timelineModel.mainThreadEvents(),
+                  filters, /* startTime */ 0, /* endTime */ Infinity, WebInspector.TimelineAggregator.eventId)
   }
 
-  bottomUp () {
+  bottomUp() {
     var topDown = this.topDown();
     var noGrouping = WebInspector.TimelineAggregator.GroupBy.None
     var noGroupAggregator =  this._aggregator.groupFunction(noGrouping)
@@ -84,7 +84,7 @@ class TraceToTimelineModel {
   }
 
   // @ returns a grouped and sorted tree
-  bottomUpGroupBy (grouping) {
+  bottomUpGroupBy(grouping) {
     var topDown = this.topDown();
     var groupSetting = WebInspector.TimelineAggregator.GroupBy[grouping] // one of: None Category Subdomain Domain URL
     var groupURLAggregator =  this._aggregator.groupFunction(groupSetting)
@@ -94,18 +94,18 @@ class TraceToTimelineModel {
     return bottomUpGrouped
   }
 
-  frameModel () {
+  frameModel() {
     var frameModel = new WebInspector.TracingTimelineFrameModel()
     frameModel.addTraceEvents({ /* target */ }, this._timelineModel.inspectedTargetEvents(), this._timelineModel.sessionId() || '')
     return frameModel
   }
 
-  filmStripModel () {
+  filmStripModel() {
     return new WebInspector.FilmStripModel(this._tracingModel)
   }
 
 
-  interactionModel () {
+  interactionModel() {
     var irModel = new WebInspector.TimelineIRModel()
     irModel.populate(this._timelineModel)
     return irModel
