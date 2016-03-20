09f784bf08f7e0e4c3a53ae1dccbe8d174912383
Paul Irish <paul.irish@gmail.com>
2016-03-19 20:40:35 -0700

    WebInspector global is now private. A bit nastier than I'd like. Apologies, everyone.

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
 /* global WebInspector TimelineModelTreeView */
 'use strict';
 
-require('./lib/api-stubs')
-
-// Pull in the devtools frontend
-require('chrome-devtools-frontend/front_end/common/Object.js')
-require('chrome-devtools-frontend/front_end/common/SegmentedRange.js')
-require('chrome-devtools-frontend/front_end/platform/utilities.js')
-require('chrome-devtools-frontend/front_end/sdk/Target.js')
-require('chrome-devtools-frontend/front_end/bindings/TempFile.js')
-require('chrome-devtools-frontend/front_end/sdk/TracingModel.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineJSProfile.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineUIUtils.js')
-require('chrome-devtools-frontend/front_end/sdk/CPUProfileDataModel.js')
-require('chrome-devtools-frontend/front_end/timeline/LayerTreeModel.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineModel.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineTreeView.js')
-require('chrome-devtools-frontend/front_end/ui_lazy/SortableDataGrid.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineProfileTree.js')
-require('chrome-devtools-frontend/front_end/components_lazy/FilmStripModel.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineIRModel.js')
-require('chrome-devtools-frontend/front_end/timeline/TimelineFrameModel.js')
-
-// local libs
-require('./lib/devtools-init')
-require('./lib/timeline-model-treeview')
+// DevTools relies on a global WebInspector variable. :(
+// This callWithGlobals BS is a nasty hack to keep this global exclusive to our model
+var callWithGlobals = require('call-with-globals');
+var _WebInspector = {}
+var _WI_global = { WebInspector: _WebInspector }
 
 class TraceToTimelineModel {
 
   constructor(events) {
-    // (devtools) tracing model
-    this._tracingModel = new WebInspector.TracingModel(new WebInspector.TempFileBackingStorage('tracing'))
-    // timeline model
-    this._timelineModel = new WebInspector.TimelineModel(WebInspector.TimelineUIUtils.visibleEventsFilter())
 
-    // populate with events
-    this._tracingModel.reset()
-    this._tracingModel.addEvents(typeof events === 'string' ? JSON.parse(events) : events)
-    this._tracingModel.tracingComplete()
-    this._timelineModel.setEvents(this._tracingModel)
+    var instance = this;
+    callWithGlobals(_ => {
 
-    this._aggregator = new WebInspector.TimelineAggregator((event) => WebInspector.TimelineUIUtils.eventStyle(event).category.name)
+      // Other globals and stubs
+      require('./lib/api-stubs')
+      // Pull in the devtools frontend
+      require('chrome-devtools-frontend/front_end/common/Object.js')
+      require('chrome-devtools-frontend/front_end/common/SegmentedRange.js')
+      require('chrome-devtools-frontend/front_end/platform/utilities.js')
+      require('chrome-devtools-frontend/front_end/sdk/Target.js')
+      require('chrome-devtools-frontend/front_end/bindings/TempFile.js')
+      require('chrome-devtools-frontend/front_end/sdk/TracingModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineJSProfile.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineUIUtils.js')
+      require('chrome-devtools-frontend/front_end/sdk/CPUProfileDataModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/LayerTreeModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineTreeView.js')
+      require('chrome-devtools-frontend/front_end/ui_lazy/SortableDataGrid.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineProfileTree.js')
+      require('chrome-devtools-frontend/front_end/components_lazy/FilmStripModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineIRModel.js')
+      require('chrome-devtools-frontend/front_end/timeline/TimelineFrameModel.js')
+      // minor configurations
+      require('./lib/devtools-init')
+      // polyfill the bottom-up and topdown tree sorting
+      require('./lib/timeline-model-treeview')
+
+      // (devtools) tracing model
+      instance._tracingModel = new WebInspector.TracingModel(new WebInspector.TempFileBackingStorage('tracing'))
+      // timeline model
+      instance._timelineModel = new WebInspector.TimelineModel(WebInspector.TimelineUIUtils.visibleEventsFilter())
+
+      // populate with events
+      instance._tracingModel.reset()
+      instance._tracingModel.addEvents(typeof events === 'string' ? JSON.parse(events) : events)
+      instance._tracingModel.tracingComplete()
+      instance._timelineModel.setEvents(instance._tracingModel)
+
+      instance._aggregator = new WebInspector.TimelineAggregator((event) => WebInspector.TimelineUIUtils.eventStyle(event).category.name)
+
+    }, _WI_global);
 
     return this;
   }
@@ -54,55 +66,82 @@ class TraceToTimelineModel {
   }
 
   topDown() {
-    var filters = [];
-    filters.push(WebInspector.TimelineUIUtils.visibleEventsFilter());
-    filters.push(new WebInspector.ExcludeTopLevelFilter());
-    var nonessentialEvents = [
-      WebInspector.TimelineModel.RecordType.EventDispatch,
-      WebInspector.TimelineModel.RecordType.FunctionCall,
-      WebInspector.TimelineModel.RecordType.TimerFire
-    ];
-    filters.push(new WebInspector.ExclusiveNameFilter(nonessentialEvents));
+    var instance = this;
+    var topDown;
+    callWithGlobals(_ => {
 
-    return WebInspector.TimelineProfileTree.buildTopDown(this._timelineModel.mainThreadEvents(),
-                  filters, /* startTime */ 0, /* endTime */ Infinity, WebInspector.TimelineAggregator.eventId)
+      var filters = [];
+      filters.push(WebInspector.TimelineUIUtils.visibleEventsFilter());
+      filters.push(new WebInspector.ExcludeTopLevelFilter());
+      var nonessentialEvents = [
+        WebInspector.TimelineModel.RecordType.EventDispatch,
+        WebInspector.TimelineModel.RecordType.FunctionCall,
+        WebInspector.TimelineModel.RecordType.TimerFire
+      ];
+      filters.push(new WebInspector.ExclusiveNameFilter(nonessentialEvents));
+
+      topDown = WebInspector.TimelineProfileTree.buildTopDown(instance._timelineModel.mainThreadEvents(),
+                    filters, /* startTime */ 0, /* endTime */ Infinity, WebInspector.TimelineAggregator.eventId)
+
+    }, _WI_global);
+    return topDown;
   }
 
   bottomUp() {
-    var topDown = this.topDown();
-    var noGrouping = WebInspector.TimelineAggregator.GroupBy.None
-    var noGroupAggregator =  this._aggregator.groupFunction(noGrouping)
-    return WebInspector.TimelineProfileTree.buildBottomUp(topDown, noGroupAggregator)
+    var instance = this;
+    var bottomUp;
+    callWithGlobals(_ => {
+      var topDown = instance.topDown();
+      var noGrouping = WebInspector.TimelineAggregator.GroupBy.None
+      var noGroupAggregator =  instance._aggregator.groupFunction(noGrouping)
+      bottomUp = WebInspector.TimelineProfileTree.buildBottomUp(topDown, noGroupAggregator)
+    }, _WI_global);
+    return bottomUp;
   }
 
   // @ returns a grouped and sorted tree
   bottomUpGroupBy(grouping) {
-    var topDown = this.topDown();
-    var groupSetting = WebInspector.TimelineAggregator.GroupBy[grouping] // one of: None Category Subdomain Domain URL
-    var groupURLAggregator =  this._aggregator.groupFunction(groupSetting)
-    var bottomUpGrouped = WebInspector.TimelineProfileTree.buildBottomUp(topDown, groupURLAggregator)
-    // sort the grouped tree, in-place
-    new TimelineModelTreeView(bottomUpGrouped).sortingChanged('self', 'desc')
+    var instance = this;
+    var bottomUpGrouped;
+    callWithGlobals(_ => {
+      var topDown = instance.topDown();
+      var groupSetting = WebInspector.TimelineAggregator.GroupBy[grouping] // one of: None Category Subdomain Domain URL
+      var groupURLAggregator =  instance._aggregator.groupFunction(groupSetting)
+      bottomUpGrouped = WebInspector.TimelineProfileTree.buildBottomUp(topDown, groupURLAggregator)
+      // sort the grouped tree, in-place
+      new TimelineModelTreeView(bottomUpGrouped).sortingChanged('self', 'desc')
+    }, _WI_global);
     return bottomUpGrouped
   }
 
   frameModel() {
-    var frameModel = new WebInspector.TracingTimelineFrameModel()
-    frameModel.addTraceEvents({ /* target */ }, this._timelineModel.inspectedTargetEvents(), this._timelineModel.sessionId() || '')
+    var instance = this;
+    var frameModel;
+    callWithGlobals(_ => {
+      frameModel = new WebInspector.TracingTimelineFrameModel()
+      frameModel.addTraceEvents({ /* target */ }, instance._timelineModel.inspectedTargetEvents(), instance._timelineModel.sessionId() || '')
+    }, _WI_global);
     return frameModel
   }
 
   filmStripModel() {
-    return new WebInspector.FilmStripModel(this._tracingModel)
+    var instance = this;
+    var fsModel;
+    callWithGlobals(_ => {
+      fsModel = new WebInspector.FilmStripModel(instance._tracingModel)
+    }, _WI_global);
+    return fsModel;
   }
-
 
   interactionModel() {
-    var irModel = new WebInspector.TimelineIRModel()
-    irModel.populate(this._timelineModel)
+    var instance = this;
+    var irModel;
+    callWithGlobals(_ => {
+      irModel = new WebInspector.TimelineIRModel()
+      irModel.populate(instance._timelineModel)
+    }, _WI_global);
     return irModel
   }
-
 }
 
 module.exports = TraceToTimelineModel
