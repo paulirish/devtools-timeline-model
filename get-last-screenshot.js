e1531271250f22c8ca6b62775dd59d5b74d276c1
Paul Irish <paul.irish@gmail.com>
2016-03-20 20:29:53 -0700

    fix getlastscreenshot test

--- get-last-screenshot.js
+++ get-last-screenshot.js
@@ -1,15 +1,15 @@
 const filename = 'test/devtools-homepage-w-screenshots-trace.json'
 
 var fs = require('fs')
-var traceToTimelineModel = require('.')
+var TraceToTimelineModel = require('.')
 
 var events = fs.readFileSync(filename, 'utf8')
-var model = traceToTimelineModel(events)
+var model = new TraceToTimelineModel(events)
 
-console.log('Filmstrip model', model.filmStripModel)
+// console.log('Filmstrip model', model.filmStripModel)
 
 // get screenshot
-var frames = model.filmStripModel.frames()
+var frames = model.filmStripModel().frames()
 var length = frames.length
 if (length >= 1) {
   frames[length - 1].imageDataPromise()
