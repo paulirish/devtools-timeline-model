c1355702da4af782cb38f3c62642217ca6d93e8a
Paul Irish <paul.irish@gmail.com>
2016-04-29 13:02:57 -0700

    migrating tests to mocha

--- test/test.js
+++ test/test.js
@@ -1,68 +1,70 @@
-import test from 'ava'
-
 var fs = require('fs')
+const assert = require('assert');
+
 var TraceToTimelineModel = require('../')
 
-const filename = 'assets/devtools-homepage-w-screenshots-trace.json'
+const filename = './test/assets/devtools-homepage-w-screenshots-trace.json'
 var events = fs.readFileSync(filename, 'utf8')
 var model
 
-test("doesn't throw an exception", (t) => {
-  t.notThrows((_) => {
-    model = new TraceToTimelineModel(events)
+describe('timeline-model basics', _ => {
+  it("doesn't throw an exception", _ => {
+    assert.doesNotThrow(_ => {
+      model = new TraceToTimelineModel(events)
+    })
   })
-})
 
-test("Array native globals dont leak", (t) => {
-  t.is(Array.prototype.peekLast, undefined)
-})
-
-test("WebInspector global doesn't leak", (t) => {
-  t.is(typeof WebInspector, 'undefined')
-})
-
-test("Multiple instances don't conflict", (t) => {
-  var model1, model2;
-  t.notThrows((_) => {
-    model1 = new TraceToTimelineModel(events)
-    model2 = new TraceToTimelineModel(events)
+  it("Array native globals dont leak", _ => {
+    assert.equal(Array.prototype.peekLast, undefined)
   })
-  var events1 = model1.timelineModel().mainThreadEvents().length;
-  var events2 = model2.timelineModel().mainThreadEvents().length;
-  t.is(events1, events2)
-})
 
-test('metrics returned are expected', (t) => {
-  t.is(model.timelineModel().mainThreadEvents().length, 7228)
-  t.is(model.interactionModel().interactionRecords().length, 0)
-  t.is(model.frameModel().frames().length, 16)
-})
+  it("WebInspector global doesn't leak", _ => {
+    assert.equal(typeof WebInspector, 'undefined')
+  })
 
-test('top-down profile', (t) => {
-  const leavesCount = model.topDown().children.size;
-  t.is(leavesCount, 28)
-  var time = model.topDown().totalTime.toFixed(2)
-  t.is(time, '559.21')
-})
+  it("Multiple instances don't conflict", _ => {
+    var model1, model2;
+    assert.doesNotThrow(_ => {
+      model1 = new TraceToTimelineModel(events)
+      model2 = new TraceToTimelineModel(events)
+    })
+    var events1 = model1.timelineModel().mainThreadEvents().length;
+    var events2 = model2.timelineModel().mainThreadEvents().length;
+    assert.equal(events1, events2)
+  })
+
+  it('metrics returned are expected', _ => {
+    assert.equal(model.timelineModel().mainThreadEvents().length, 7228)
+    assert.equal(model.interactionModel().interactionRecords().length, 0)
+    assert.equal(model.frameModel().frames().length, 16)
+  })
+
+  it('top-down profile', _ => {
+    const leavesCount = model.topDown().children.size;
+    assert.equal(leavesCount, 28)
+    var time = model.topDown().totalTime.toFixed(2)
+    assert.equal(time, '559.21')
+  })
 
 
-test('bottom-up profile', (t) => {
-  const leavesCount = model.bottomUp().children.size;
-  t.is(leaves, 243)
-  var topCosts = [...model.bottomUpGroupBy('URL').children.values()]
-  var time = topCosts[1].totalTime.toFixed(2)
-  var url = topCosts[1].id
-  t.is(time, '80.77')
-  t.is(url, 'https://s.ytimg.com/yts/jsbin/www-embed-lightweight-vflu_2b1k/www-embed-lightweight.js')
-})
+  it('bottom-up profile', _ => {
+    const leavesCount = model.bottomUp().children.size;
+    assert.equal(leaves, 243)
+    var topCosts = [...model.bottomUpGroupBy('URL').children.values()]
+    var time = topCosts[1].totalTime.toFixed(2)
+    var url = topCosts[1].id
+    assert.equal(time, '80.77')
+    assert.equal(url, 'https://s.ytimg.com/yts/jsbin/www-embed-lightweight-vflu_2b1k/www-embed-lightweight.js')
+  })
 
-test('bottom-up profile - group by eventname', (t) => {
-  const bottomUpByName = model.bottomUpGroupBy('EventName');
-  const leavesCount = bottomUpByName.children.size;
-  t.is(leavesCount, 15)
-  const topCosts = [...bottomUpByName.children.values()];
-  const time = topCosts[0].selfTime.toFixed(2);
-  const name = topCosts[0].id;
-  t.is(time, '187.75')
-  t.is(name, 'Layout')
-})
+  it('bottom-up profile - group by eventname', _ => {
+    const bottomUpByName = model.bottomUpGroupBy('EventName');
+    const leavesCount = bottomUpByName.children.size;
+    assert.equal(leavesCount, 15)
+    const topCosts = [...bottomUpByName.children.values()];
+    const time = topCosts[0].selfTime.toFixed(2);
+    const name = topCosts[0].id;
+    assert.equal(time, '187.75')
+    assert.equal(name, 'Layout')
+  })
+});
